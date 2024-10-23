0f3a264e8f3be915ab5dbec1d0a7ff098a9de265
kadir-zeren <k4dzeren@gmail.com>
2024-10-23 09:24:44 +0300

    practice

--- python.py
+++ python.py
@@ -4,3 +4,52 @@ print(school)
 sayi = 35
 print(sayi)
 
+my_age = 33
+your_age = 30
+my_age = your_age
+print(my_age)
+
+variable = 3.14
+
+help('keywords')
+
+x = y = z = 'SAME'
+
+print(x,y,z)
+
+ali = None
+print(ali)
+
+x = 15
+y = 33 
+z = x
+x = y
+print(x)
+print(y)
+print(z)
+
+a = 'clarusway'
+type(a)
+
+
+sayi1 = 40
+type(sayi1)
+
+b = -1
+print(type(b))
+
+a = 123
+print(type(a))
+
+yeni_a = str(a)
+print(type(yeni_a))
+
+x = 39
+v = '11'
+y = '2.5'
+z = "I am at_"
+print(x-int(v)) 
+print(x-float(y)) 
+print(z+str(x)) 
+
+
